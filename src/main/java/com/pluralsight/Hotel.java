540b2f5f2b810588ea96e69ec7b79c297ad3811d
ahmadbelalsidiqi <Ahmad.sidiqiNe@gmail.com>
2026-05-05 15:20:11 -0400

    Hotel class- bookRoom,getAvailableSuites & getAvailableRoom methods added.

---
+++ src/main/java/com/pluralsight/Hotel.java
@@ -0,0 +1,51 @@
+package com.pluralsight;
+
+public class Hotel {
+    private final String name;
+    private final int numberOfSuites;
+    private final int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = 0;
+        this.bookedBasicRooms = 0;
+
+
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+        if (isSuite && numberOfRooms <= getAvailableSuites()) {
+            this.bookedSuites += numberOfRooms;
+            return true;
+        }
+        if (numberOfRooms <= getAvailableRoom()) {
+            this.bookedBasicRooms += numberOfRooms;
+            return true;
+        }
+        return false;
+
+    }
+
+    public int getAvailableSuites() {
+        return this.numberOfSuites - this.bookedSuites;
+    }
+
+    public int getAvailableRoom() {
+
+        return this.numberOfRooms - this.bookedBasicRooms;
+    }
+}
